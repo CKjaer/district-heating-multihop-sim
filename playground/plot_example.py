a7ce280da82e740aeb18c7ab2c185121321c7eb3
CKjaer <ChristianKjaerNielsen@gmail.com>
2026-08-27 14:28:18 +0200

    Add spell checker and ruff to project

--- playground/plot_example.py
+++ playground/plot_example.py
@@ -7,17 +7,21 @@ sns.set_theme(style="whitegrid")
 # Render LaTex in plots
 # plt.rcParams.update({
 #     "text.usetex": True,
-#     "font.family": "serif",
+# "font.family": "serif",
 # })
 
 
-x = np.linspace(0, 2 * np.pi, 200)
+x = np.linspace(
+    0,
+    2 * np.pi,
+    200,
+)
 y = np.sin(x)
 
 ax = sns.lineplot(x=x, y=y)
 ax.set_xlabel(r"$x$")
 ax.set_ylabel(r"$\sin(x)$")
-ax.set_title(r"Simple $x$–$y$ plot: $y = \sin(x)$")
+ax.set_title(r"Simple $x$-$y$ plot: $y = \sin(x)$")
 
 plt.tight_layout()
 # plt.savefig("plot.png", dpi=300, bbox_inches="tight")
